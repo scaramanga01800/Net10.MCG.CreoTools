b7f9a80192f23072596a7edd7dcdd4a5d47844fb
Thierry Champier <thierry.champier@gmail.com>
2026-09-02 12:58:05 +0200

    Revert "Correction faille sécurité site HTTP"
    This reverts commit 2bcf7deba2e5ca513f357ee7758153d7821da04d.

--- MCG.CREO_Tools.QuickLaunch/Configuration/QuickLaunchConstants.cs
+++ MCG.CREO_Tools.QuickLaunch/Configuration/QuickLaunchConstants.cs
@@ -3,9 +3,9 @@
     public static class QuickLaunchConstants
     {
         public const string ConfigurationFile = "QuickLaunchConfiguration.xml";
-        public const string HtmlLinkEngTime = "https://gdeweb002/ENG-Time/ENG_Time.asp";
+        public const string HtmlLinkEngTime = "http://gdeweb002/ENG-Time/ENG_Time.asp";
         public const string HtmlLinkMcgDocumentation = "https://manitowoc.sharepoint.com/sites/TW_EMEA_ENG_ENGSERVICES/CAD%20Eng%20Documents/Forms/Par%20Applications.aspx";
-        public const string HtmlLinkPatNumberGenerator = "https://gusang002/AutoPartNumberGenerator/";
+        public const string HtmlLinkPatNumberGenerator = "http://gusang002/AutoPartNumberGenerator/";
         public const string HtmlLinkPresisionLms = "https://precisionlms.ptc.com/";
         public const string HtmlLinkWebterm = "https://webterm.manitowoc.com/WebTerm/UI7/login.xsl";
         public const string HtmlLinkOpenCall = "https://manitowoc.service-now.com/";
